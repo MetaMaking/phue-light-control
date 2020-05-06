a8248c43843863f51ae9a1644a40c228998e14c7
Cesar Garcia <cesar@lahoramaker.com>
2020-05-06 17:54:44 +0200

    First Commit!

---
+++ lights_on.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from phue import Bridge
+
+b = Bridge('192.168.1.124')
+b.connect()
+
+lights_list = b.get_light_objects('list')
+
+for light in lights_list:
+   light.on = True
+   light.brightness = 255
